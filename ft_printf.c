2f008ed8bbbcad0fdebc19736bc9c330e806a5df
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-10-04 21:28:56 +0900

    updated ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,14 +6,17 @@
 /*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 12:17:11 by hmitsuyo          #+#    #+#             */
-/*   Updated: 2023/10/04 19:49:25 by hmitsuyo         ###   ########.fr       */
+/*   Updated: 2023/10/04 21:27:49 by hmitsuyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_printf(const char *format, ...)
 {
 	va_list	ap;
+	int	result;
 
 	va_start(ap, arg_1);
-
+	
+	va_end(ap);
+	return ();
 }
