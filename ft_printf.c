7f77340b9f523dc129eadf45553ddd3f70fc12ca
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-10-09 16:59:06 +0900

    updated

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,26 @@
 /*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 12:17:11 by hmitsuyo          #+#    #+#             */
-/*   Updated: 2023/10/06 04:58:40 by hmitsuyo         ###   ########.fr       */
+/*   Updated: 2023/10/09 07:08:41 by hmitsuyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	print_result()
+int	print_result(const char *format, va_list ap)
 {
+	int	count;
+	int	i;
+
+	i = 0;
+	count = 0;
+	while (format = '\0')
+	{
+		write(1, format[i], 1);
+		if (format[i] = '%')
+		{
+			count++;
+		}
+		i++;
+	}
 }
 
 int	ft_printf(const char *format, ...)
@@ -19,8 +33,10 @@ int	ft_printf(const char *format, ...)
 	va_list	ap;
 	int	result;
 
+	if (!format)
+		return (0);
 	va_start(ap, arg_1);
-	result = print_result();
+	result = print_result(format, ap);
 	va_end(ap);
 	return (result);
 }
--- test.c
+++ test.c
@@ -2,14 +2,9 @@
 
 int main (void)
 {
-//	printf("%d\n", 123);
-	printf("%i\n", 123);
+	int re;
+	re = printf();
 
-	char *p = "hello";
-	int i = 0;
-
-	printf("%c\n", p[i]);
-	printf("%c\n", i[p]);
-	i++;
+	printf("%d\n", re);
 	return (0);
 }
