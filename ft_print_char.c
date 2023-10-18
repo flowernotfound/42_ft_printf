2dad8e673907f0e156742a2bd0e4fd8aa44a1772
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-10-19 01:50:21 +0900

    fixed print_char

--- ft_print_char.c
+++ ft_print_char.c
@@ -6,7 +6,7 @@
 /*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 16:56:43 by hmitsuyo          #+#    #+#             */
-/*   Updated: 2023/10/17 17:10:21 by hmitsuyo         ###   ########.fr       */
+/*   Updated: 2023/10/19 00:56:38 by hmitsuyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,24 @@
 int	print_char(int c, t_flags flags)
 {
 	int	count;
+	char	padding;
 
-	count = 0;
-	write(1, &c, 1);
-	count++;
+	if (flags.zero_padding && !flags.left_justify)
+		padding = '0';
+	else
+		padding = ' ';
+	if (flags.left_justify)
+	{
+		write(1, &c, 1);
+		count++;
+		while (count < flags.width)
+		{
+			write(1, &padding, 1);
+			count++;
+		}
+	}
+	else
+	{
+
+	}
 }
--- test.c
+++ test.c
@@ -5,7 +5,7 @@ int main (void)
 	int a = 123;
 	int count = 0;
 	char *s = "Hello";
-	count = printf("[%--10d]\n", a);
+	count = printf("[%10c]\n", 'a');
 
 	printf("%d\n", count);
 	return (0);
