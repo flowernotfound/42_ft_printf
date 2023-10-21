1b420eee4e6689059ab34fc795ae62e8eb6e3fc7
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-10-21 19:06:37 +0900

    updated print_str

--- ft_print_str.c
+++ ft_print_str.c
@@ -6,12 +6,32 @@
 /*   By: hmitsuyo <yourLogin@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 02:27:09 by hmitsuyo          #+#    #+#             */
-/*   Updated: 2023/10/20 02:28:05 by hmitsuyo         ###   ########.fr       */
+/*   Updated: 2023/10/21 19:06:21 by hmitsuyo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static int	print_str_left(char *s, t_flags flags, char padding)
+{
+}
+
+static int	print_str_right(char *s, t_flags flags, char padding)
+{
+}
+
 int	print_str(char *s, t_flags flags)
 {
+	char	padding;
+
+	if (!s)
+		s = "(null)";
+	if (flgas.zero_padding && !flags.left_justify)
+		padding = '0';
+	else
+		padding = ' ';
+	if (flags.left_justify)
+		return (print_str_left(s, flags, padding));
+	else
+		return (print_str_right(s, flgas, padding));
 }
